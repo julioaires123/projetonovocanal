7c1807f80cb164f58f54630c6b364216f53fa3f6
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2022-10-03 21:38:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//horário de Brasília////////////////////////////////////////////////////////////////////////////////////////
-    setInterval(function relog() {
+        setInterval(function relog() {//horário de Brasília////////////////////////////////////////////////////////////////////////////////////////
         let rel = document.getElementById('relogio01')
        
         let data = new Date();
@@ -7,32 +6,35 @@
         let m = data.getMinutes();
         let s = data.getSeconds()+30;
 
-        //Aumenta + 1 min, hora, segundos quando ele chega 00 segundos sem esse if ele só vira nos 30 segunstos do tempo
-
-        if (s > 59){
+        if (s > 59){ //Aumenta + 1 min, hora, segundos quando ele chega 00 segundos sem esse if ele só vira nos 30 segunstos do tempo
          m = m + 1;
     
         }
 
-         if (m > 59){
-         h = h + 1;
-        
+        if (h === 24) {
+
+         h = 00;
         }
-         // Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
-            if (s >= 60 && s <= 89){
+ 
+        if (m === 60) {
+ 
+         m = 00;
+        }
+
+         if (m > 59){
+         h = h + 1;   
+         }
+
+            if (s >= 60 && s <= 89){// Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
                 s = s - 60
             }
 
-         // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00
-        
-          if (h > 24){
+          if (h > 24){ // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00
         
            h = h - 24
           }
 
-          // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-        
-          if (h < 10) {
+          if (h < 10) {// coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
            h = `0${h}`
         }
         
@@ -44,62 +46,50 @@
            s = `0${s}`
         }
         // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
-            rel.innerHTML = `${h}:${m}:${s}`
-             
+            rel.innerHTML = `${h}:${m}:${s}` 
         }, 1000)
-
-    //horário de Fernando de Noronha////////////////////////////////////////////////////////////////////////
-    
-    setInterval(function relog2() {
+    setInterval(function relog2() {//horário de Fernando de Noronha////////////////////////////////////////////////////////////////////////
         let rel = document.getElementById('relogio2')
         let data = new Date();
         let h = data.getHours() + 1; //<< aumentou 1 hora No Fuso-Horario de Fernando de Noronha
         let m = data.getMinutes();
         let s = data.getSeconds() + 30;  //<< aumentou 30 segundos 
 
-         //Aumenta + 1 min, hora, segundos quando ele chega 00 segundos sem esse if ele só vira nos 30 segunstos do tempo
-
-         if (s > 59){
+         if (s > 59){ //Aumenta + 1 min, hora, segundos quando ele chega 00 segundos sem esse if ele só vira nos 30 segunstos do tempo
             m = m + 1;
-       
            }
    
+           if (h === 24) {
+           h = 00;
+           }
+    
+           if (m === 60) {
+            m = 00;
+           }
+
             if (m > 59){
             h = h + 1;
-           
-           }
-            // Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
-               if (s >= 60 && s <= 89){
+            }
+               if (s >= 60 && s <= 89){// Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
                    s = s - 60
-               }
-   
-            // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00
-           
-             if (h > 24){
+               }  
+             if (h > 24){  // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00
            
               h = h - 24
              }
-   
-             // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-           
-             if (h < 10) {
+             if (h < 10) {  // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
               h = `0${h}`
-           }
-           
+           } 
            if (m < 10) {
               m = `0${m}`
            }
-           
            if (s < 10) {
               s = `0${s}`
            }
            // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
-               rel.innerHTML = `${h}:${m}:${s}` 
-           
+               rel.innerHTML = `${h}:${m}:${s}`
            }, 1000)
-
-    //horário de Amazonas///////////////////////////////////////////////////////////////////////////////////   
-    setInterval(function relog3() {
+    setInterval(function relog3() {  //horário de Amazonas///////////////////////////////////////////////////////////////////////////////////   
         let rel = document.getElementById('relogio3')
         let data = new Date();
         let h = data.getHours() + 23; //<< Diminuiu 1 hora Fuso-Horario do Amazonas
@@ -109,109 +99,94 @@
         //Aumenta + 1 min, hora, segundos quando ele chega 00 segundos sem esse if ele só vira nos 30 segunstos do tempo
 
         if (s > 59){
-            m = m + 1;
-       
-           }
-   
-            if (m > 59){
-            h = h + 1;
-           
-           }
-            // Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
-               if (s >= 60 && s <= 89){
-                   s = s - 60
-               }
-   
-            // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00
-           
-             if (h > 24){
-           
-              h = h - 24
-             }
-             // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-           
-             if (h < 10) {
-              h = `0${h}`
-           }
-           
-           if (m < 10) {
-              m = `0${m}`
-           }
-           
-           if (s < 10) {
-              s = `0${s}`
-           }
-           // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
-               rel.innerHTML = `${h}:${m}:${s}`
-           
-           
+         m = m + 1;
+        }
+
+        if (h === 24) {
+         h = 00;
+        }
+ 
+        if (m === 60) {
+         m = 00;
+        }
+
+         if (m > 59){
+         h = h + 1;
+         }
+            if (s >= 60 && s <= 89){// Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
+                s = s - 60
+            }
+
+          if (h > 24){  // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00
+           h = h - 24
+          }
+
+          if (h < 10) {  // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
+           h = `0${h}`
+        }
+        
+        if (m < 10) {
+           m = `0${m}`
+        }
+        
+        if (s < 10) {
+           s = `0${s}`
+        }
+            rel.innerHTML = `${h}:${m}:${s}`   // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
            }, 1000)
-    
-    //horário de Acre////////////////////////////////////////////////////////////////////////////////////
-    
-    setInterval(function relog4() {
+        setInterval(function relog4() {  //horário de Acre/////////////////////////////////////////////////////////////////////////////////////////////////////
         let rel = document.getElementById('relogio4')
         let data = new Date();
         let h = data.getHours() + 22; //<< Diminuiu 2 hora Fuso-Horario do Acre
         let m = data.getMinutes();
         let s = data.getSeconds() + 30;   //<< aumentou 30 segundos 
-    
-         //Aumenta + 1 min, hora, segundos quando ele chega 00 segundos sem esse if ele só vira nos 30 segunstos do tempo
+  
+        if (s > 59){
+         m = m + 1;
+        }
 
-         if (s > 59){
-            m = m + 1;
+        if (h === 24) {
+         h = 00;
+        }
+ 
+        if (m === 60) {
+         m = 00;
+        }
+
+         if (m > 59){
+         h = h + 1;
+        }
+            if (s >= 60 && s <= 89){ // Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
+                s = s - 60
+            }
+          if (h > 24){ // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00 
+           h = h - 24
+          }
+          if (h < 10) { // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
+           h = `0${h}`
+        }
+        
+        if (m < 10) {
+           m = `0${m}`
+        }
        
-           }
-   
-            if (m > 59){
-            h = h + 1;
-           
-           }
-            // Quando aumenta os +30 segundos ele vai contando de 60 segundos a 89 segundos assim com esse if ele arruma.
-               if (s >= 60 && s <= 89){
-                   s = s - 60
-               }
-   
-            // Deixa a hora do Fuso-Horario certo sem o código ele fica 24:00:00 com o código ele fica 00:00:00
-           
-             if (h > 24){
-           
-              h = h - 24
-             }
-   
-             // coloca um zero antes dos números abaixo de dez: ex: 1: 1: 1 depois ex 01:01:01////////////////////
-           
-             if (h < 10) {
-              h = `0${h}`
-           }
-           
-           if (m < 10) {
-              m = `0${m}`
-           }
-           
-           if (s < 10) {
-              s = `0${s}`
-           }
-           // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
-               rel.innerHTML = `${h}:${m}:${s}`
-           
-           }, 1000)
-    
+        if (s < 10) {
+           s = `0${s}`
+        }
+            rel.innerHTML = `${h}:${m}:${s}`  // Mostra a hora minutos e segundos na página /////////////////////////////////////////////////////
+           }, 1000)  
 //Dia mês e ano /////////////////////////////////////////////////////////////////////////////////////////////
-
-let meses = new Array("Dezembro", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Julho", "Agosto", "Setembro", "Outubro", "Novembro")
+    let meses = new Array("Dezembro", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Julho", "Agosto", "Setembro", "Outubro", "Novembro")
     let semanas = new Array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado")
-
     let rel = document.getElementById('date')
-
     let data = new Date();
     let diasem = data.getDay();
     let dia = data.getDate();
     let mes = data.getMonth();
     let ano = data.getFullYear();
-   
     let h = data.getHours();
     let m = data.getMinutes();
     let s = data.getSeconds();
-
     document.write(semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano);
+
+ 
