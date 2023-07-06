735228c06fd0651da7ac72a50f0ac176a67763a8
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-06 19:47:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -97,23 +97,6 @@ function exibirDataAtualizada() {
   let mes = data.getMonth();
   let ano = data.getFullYear();
 
-  // Verifica se é meia-noite (00:00:00)
-  if (data.getHours() === 0 && data.getMinutes() === 0 && data.getSeconds() === 0) {
-    // Incrementa um dia
-    data.setDate(data.getDate() + 1);
-    dia = data.getDate();
-
-    // Verifica se é necessário atualizar o mês
-    if (dia === 1) {
-      mes = data.getMonth();
-    }
-
-    // Verifica se é necessário atualizar o ano
-    if (dia === 1 && mes === 0) {
-      ano = data.getFullYear();
-    }
-  }
-
   // Atualiza o conteúdo do elemento com o ID "date"
   document.getElementById("date").innerHTML = semanas[diasem] + ", " + dia + " de " + meses[mes] + " de " + ano;
 }
@@ -122,6 +105,7 @@ function exibirDataAtualizada() {
 function incrementarDia() {
   let data = new Date();
   data.setDate(data.getDate() + 1);
+  exibirDataAtualizada();
   setTimeout(incrementarDia, 5000); // Chama a função novamente após 5 segundos
 }
 
