5c20e7acd1766131791adf48b0f688cbc41837ec
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-07-06 18:43:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -83,10 +83,12 @@
 // Função para exibir a data atualizada
 function exibirDataAtualizada() {
   let meses = [
-    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho","Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
   ];
   let semanas = [
-    "Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira","Quinta-Feira", "Sexta-Feira", "Sábado"
+    "Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira",
+    "Quinta-Feira", "Sexta-Feira", "Sábado"
   ];
 
   let data = new Date();
@@ -95,20 +97,13 @@ function exibirDataAtualizada() {
   let mes = data.getMonth();
   let ano = data.getFullYear();
 
-  // Verifica se é meia-noite (00:00:20)
+  // Verifica se é meia-noite (00:00:00)
   if (data.getHours() === 0 && data.getMinutes() === 0 && data.getSeconds() === 0) {
-    data.setDate(data.getDate();
+    // Incrementa um dia
+    data.setDate(data.getDate() + 1);
     dia = data.getDate();
-
-    // Verifica se é necessário atualizar o mês
-    if (dia === 1) {
-      mes = data.getMonth();
-    }
-
-    // Verifica se é necessário atualizar o ano
-    if (dia === 1 && mes === 0) {
-      ano = data.getFullYear();
-    }
+    mes = data.getMonth();
+    ano = data.getFullYear();
   }
 
   // Atualiza o conteúdo do elemento com o ID "date"
@@ -117,13 +112,33 @@ function exibirDataAtualizada() {
 
 // Função para atualizar a data a cada segundo
 function atualizarData() {
-  exibirDataAtualizada();
-  setTimeout(atualizarData, 1000); // Chama a função novamente após 1 segundo
+  let data = new Date();
+  let horas = data.getHours();
+  let minutos = data.getMinutes();
+  let segundos = data.getSeconds();
+
+  // Verifica se é meia-noite (00:00:00)
+  if (horas === 0 && minutos === 0 && segundos === 0) {
+    exibirDataAtualizada();
+
+    // Define o próximo intervalo de atualização para o próximo dia
+    let proximaAtualizacao = new Date();
+    proximaAtualizacao.setDate(proximaAtualizacao.getDate() + 0);
+    proximaAtualizacao.setSeconds(proximaAtualizacao.getSeconds() + 20);      
+    proximaAtualizacao.setHours(0);
+    proximaAtualizacao.setMinutes(0);
+    proximaAtualizacao.setSeconds(0);
+
+    let tempoAteProximaAtualizacao = proximaAtualizacao.getTime() - data.getTime();
+
+    setTimeout(atualizarData, tempoAteProximaAtualizacao);
+  } else {
+    setTimeout(atualizarData, 1000); // Chama a função novamente após 1 segundo
+  }
 }
 
 // Chama a função para exibir a data atualizada
 exibirDataAtualizada();
 
-// Aguarda 20 segundos para iniciar a atualização a cada segundo
-setTimeout(atualizarData, 10000);
-
+// Atualiza a data a cada segundo
+atualizarData();
