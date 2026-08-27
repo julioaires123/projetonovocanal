f1d1e51f966749169c9e5525bbbf62d7332bc69e
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2026-08-27 09:26:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // DEFINE O DELAY DA LIVE EM SEGUNDOS (ex: 15 segundos de atraso = 15)
-const DELAY_LIVES_SEGUNDOS = 19; 
+const DELAY_LIVES_SEGUNDOS = 20; 
 
 function obterHoraFormatada(timeZone) {
     const agora = new Date();
